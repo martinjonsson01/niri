37cb21df795c4560a94bbd0dc567636fc2b85389
Martin <martinjonsson01@gmail.com>
2025-07-21 10:01:06 +0200

    Add xx-session-management-v1 protocol boilerplate

--- src/protocols/raw.rs
+++ src/protocols/raw.rs
@@ -23,3 +23,29 @@ pub mod mutter_x11_interop {
         }
     }
 }
+
+#[cfg(feature = "xx-session-management")]
+pub mod xx_session_management {
+    pub mod v1 {
+        pub use self::generated::server;
+
+        mod generated {
+            pub mod server {
+                #![allow(dead_code, non_camel_case_types, unused_unsafe, unused_variables)]
+                #![allow(non_upper_case_globals, non_snake_case, unused_imports)]
+                #![allow(missing_docs, clippy::all)]
+
+                use smithay::reexports::wayland_protocols::xdg::shell::server::xdg_toplevel;
+                use smithay::reexports::wayland_server;
+
+                pub mod __interfaces {
+                    use smithay::reexports::wayland_protocols::xdg::shell::server::__interfaces::*;
+                    wayland_scanner::generate_interfaces!("resources/xx-session-management-v1.xml");
+                }
+                use self::__interfaces::*;
+
+                wayland_scanner::generate_server_code!("resources/xx-session-management-v1.xml");
+            }
+        }
+    }
+}
